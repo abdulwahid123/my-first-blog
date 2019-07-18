f210edc71f44551f3a95c63ea665f35e89d3ee7c
abdulwahid123 <tunad123@gmail.com>
2019-07-18 23:50:35 +0100

    Modified template tp display

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,12 +8,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
-if path not in sys.path:
-    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
